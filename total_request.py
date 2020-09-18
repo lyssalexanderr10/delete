3197eb1f40ac85b7c5daa0267a3b4d0a1e6a6dae
Alyssa Alexander <alyssa-alexander10@tamu.edu>
2020-09-18 00:49:21 -0500

    Update total_request.py

--- total_request.py
+++ total_request.py
@@ -4,6 +4,7 @@ from collections import Counter
 
 total_count = 0
 
+for line in open(FILE_NAME):
     total_count = total_count + 1
 
 print(f"There was {total_count} requests made in the time period represented by the log.")
